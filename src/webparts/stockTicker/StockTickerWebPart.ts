b63c92548c9c21c57c24ee938774527c20bfd9bf
Jonathan Batselos <jbatselos@withum.com>
2019-02-21 21:10:59 -0500

    UI only

---
+++ src/webparts/stockTicker/StockTickerWebPart.ts
@@ -0,0 +1,73 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField,
+  PropertyPaneDropdown,
+  PropertyPaneFieldType
+} from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './StockTickerWebPart.module.scss';
+import * as strings from 'StockTickerWebPartStrings';
+
+export interface IStockTickerWebPartProps {
+  description: string;
+  symbol: string;
+}
+
+export default class StockTickerWebPart extends BaseClientSideWebPart<IStockTickerWebPartProps> {
+
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${ styles.stockTicker }">
+        <div class="${ styles.container }">
+          <div class="${ styles.row }">
+            <div class="${ styles.column }">
+              <span class="${ styles.title }">Stock Ticker</span>
+              <p class="${ styles.subTitle }">Name:     Symbol: ${escape(this.properties.symbol)}</p>
+              <p class="${ styles.subTitle }">Price: Change: PerChange:</p>
+              <p class="${ styles.description }">Date</p>
+              <a href="https://api.iextrading.com/1.0/stock/${escape(this.properties.symbol)}/book" class="${ styles.button }">
+                <span class="${ styles.label }">See API</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription,
+          },
+          groups: [
+            {
+              //groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneDropdown('symbol', {
+                  label: "Stock Symbol", selectedKey: 'AAPL',
+                  options: [
+                    { key: 'AAPL', text: 'AAPL' },
+                    { key: 'MSFT', text: 'MSFT' },
+                    { key: 'GOOGL', text: 'GOOGL' },
+                    { key: 'AMZN', text: 'AMZN' }
+                  ]})
+                
+
+                
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
